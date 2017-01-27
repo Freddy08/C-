85c313774e5325c47d5b8027c3bb409e4227bc8c
Freddy Serrano Ochoa <ferrariunach@outlook.es>
2017-01-27 16:53:42 -0600

    Ejercicio de C#
    Ejercicio de lectura de cadenas en C#

---
+++ Leer Cadena y Comparar/Leer Cadena y Comparar/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leer_Cadena_y_Comparar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cadena;
+            char[]dic = new char[17]{'0','1','2','3','4','5','6','7','8','9','+','-','*','/','(',')','='};
+            Console.Write("Introducir la cadena: ");
+            cadena = Console.ReadLine();
+            int ciclo, veces;
+            int total=0;
+            for (veces = 0; veces < cadena.Length; veces++)
+            {
+                for (ciclo = 0; ciclo < 17; ciclo++)
+                {
+                    if (char.Parse(cadena.Substring(veces, 1)) == dic[ciclo])
+                    {
+                        //suma
+                        total++;
+                        break;
+                    }
+                }
+            }
+            if (total == cadena.Length)
+            {
+                Console.WriteLine("Cadena valida");
+            }
+            else {
+                Console.WriteLine("Error Lexico");
+            }
+            Console.ReadLine();
+        }
+    }
+}
